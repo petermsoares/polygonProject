4d491f8e02c89c23148b7003376aa1c5cbc5c785
Peter <petersoares52@gmail.com>
2022-09-13 13:16:37 -0400

    figured out the basic idea of animating CSS elements with javascript. Need to figure out how to offset

---
+++ tying JS animations/app.js
@@ -0,0 +1,41 @@
+const blocks = document.querySelectorAll(".block")
+const blockSection = document.querySelectorAll(".section2")
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return
+        }
+        else{
+            blocks.forEach(block => {
+            block.classList.add("animate")
+            })          
+    }
+    })
+})
+
+blockSection.forEach(section => {
+    observer.observe(section)
+})
+
+
+// const blocks = document.querySelectorAll(".block")
+// const blockSection = document.querySelector("section2")
+
+// const observer = new IntersectionObserver(entry => {
+//     if(!entry.isIntersecting){
+//         return
+//     }
+//     else{
+//         blocks.classList.add("animate")
+//     }
+
+
+//     // entries.forEach(entry =>{
+//     //     entry.target.classList.toggle("animate", entry.isIntersecting)
+//     // })
+// })
+
+// // blockSection.forEach(block => {
+// //     observer.observe(block)
+// // })
