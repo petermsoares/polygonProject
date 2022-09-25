9323e81a8f35603d8dcbc5b9b0848b8fd216178a
Peter <petersoares52@gmail.com>
2022-09-25 18:32:23 -0400

    JavaScript Zenitsu Animation Fix I decided I didn't like how the lightning strikes looked and I'm going to redesign the animation sequence that reveals the Zenitsu character.
    Instead of having lightning strikes I am going to
    animate a polygon line with a trim-paths effect
    running up/down the character outlining the major
    shapes of the character.
    
    The way this effect works is as follows: Each line
    will have a designated CSS class that contains the
    animated line of the polygon. Each one is unique.
    These CSS classes are added to each respective div
    element using a for loop.

--- app.js
+++ app.js
@@ -1,51 +1,62 @@
-const viPolygons = document.querySelectorAll(".viPolygon")
-const viPolygonSection = document.querySelectorAll(".polygon222")
-const viDescription = document.querySelectorAll(".viDescription")
+// const viPolygons = document.querySelectorAll(".viPolygon")
+// const viPolygonSection = document.querySelectorAll(".polygon222")
+// const viDescription = document.querySelectorAll(".viDescription")
+
+const zenitsu_Animations = ["line-animation-1","line-animation-2"]
+const zenitsu_Shapes = document.querySelectorAll(".shape")
+const zArray = Array.from(zenitsu_Shapes)
+console.log(zArray)
+
+for (let i = 0; i < zArray.length; i++){
+    console.log(zenitsu_Animations[i])
+    zArray[i].classList.add(zenitsu_Animations[i])
+}
 
 
-const observerViPolygon = new IntersectionObserver(entries => {
-    entries.forEach(entry =>{
-        if(!entry.isIntersecting){
-            return
-        }
-        else{    
+
+// const observerViPolygon = new IntersectionObserver(entries => {
+//     entries.forEach(entry =>{
+//         if(!entry.isIntersecting){
+//             return
+//         }
+//         else{    
             
-            viPolygons.forEach((piece, i) => { 
-                setTimeout(() => {               
-            piece.classList.add("viPolygonAnimate")
-            }, i * 20);
-            
-        
-        })      
-    }
-    })
-})
-
-viPolygonSection.forEach(section => {
-    observerViPolygon.observe(section)
-})
-
-const observerViDescription = new IntersectionObserver(entries => {
-    entries.forEach(entry =>{
-        if(!entry.isIntersecting){
-            return
-        }
-        else{    
-            
-            viDescription.forEach((description) => { 
-                setTimeout(() => {               
-            description.classList.add("viDescriptoin__Fade")
-            }, 6500);
+//             viPolygons.forEach((piece, i) => { 
+//                 setTimeout(() => {               
+//             piece.classList.add("viPolygonAnimate")
+//             }, i * 20);
             
         
-        })      
-    }
-    })
-})
+//         })      
+//     }
+//     })
+// })
 
-viPolygonSection.forEach(section => {
-    observerViDescription.observe(section)
-})
+// viPolygonSection.forEach(section => {
+//     observerViPolygon.observe(section)
+// })
+
+// const observerViDescription = new IntersectionObserver(entries => {
+//     entries.forEach(entry =>{
+//         if(!entry.isIntersecting){
+//             return
+//         }
+//         else{    
+            
+//             viDescription.forEach((description) => { 
+//                 setTimeout(() => {               
+//             description.classList.add("viDescriptoin__Fade")
+//             }, 6500);
+            
+        
+//         })      
+//     }
+//     })
+// })
+
+// viPolygonSection.forEach(section => {
+//     observerViDescription.observe(section)
+// })
 
 
 
